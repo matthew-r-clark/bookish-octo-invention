e758adf1bb72e3f0a87875c3cbd6295030addfeb
Matt Clark <clark.matthewr@gmail.com>
2020-10-15 14:17:30 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+$(function() {
+  $('#location').text(location.href);
+
+  let generateLink = (() => {
+    let id = Math.round(Math.random() * (Math.random() * 100));
+    return function() {
+      id += 1;
+      return `<img src="https://picsum.photos/id/${id}/120/150" />`;
+    }
+  })();
+
+  Array.prototype.slice.call($('ul.list-items').children()).forEach(li => $(li).html(generateLink()));
+});
