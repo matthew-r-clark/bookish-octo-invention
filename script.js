07fbd5faac19ab67af6fc9d99a15923f20571857
Matt Clark <clark.matthewr@gmail.com>
2020-10-15 16:49:41 -0500

    fix issue with some pictures not loading

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ $(function() {
     let id = Math.round(Math.random() * (Math.random() * 100));
     return function() {
       id += 1;
+      if (id === 86 || id === 97) { id += 1; }
       return `<img src="https://picsum.photos/id/${id}/120/150" />`;
     }
   })();
